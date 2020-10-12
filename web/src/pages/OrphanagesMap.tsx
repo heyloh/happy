198677326bc56919c55416779f8c59a3bb688941
Lohana Torres <lohana100739@gmail.com>
2020-10-12 16:34:16 -0300

    Edit map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -29,11 +29,11 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%' }}
       >
         
-        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         
-        <TileLayer 
+        {/* <TileLayer 
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
-        />
+        /> */}
       
       </Map>
       <Link to="" className="create-orphanage">
