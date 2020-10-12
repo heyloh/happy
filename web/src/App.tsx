de22d3a37b5891b11b40e90b7dbeee70d34f3eb8
Lohana Torres <lohana100739@gmail.com>
2020-10-12 15:08:58 -0300

    Add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoImg from './images/logo.svg';
+import Routes from './routes';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="happy"/>
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-        <div className="location">
-          <strong>Rio de Janeiro</strong>
-          <span>Rio de Janeiro</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Landing} exact/>
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+export default Routes;
