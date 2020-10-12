cdd4c1db11df1164657a2f51a4aad04042bcdaca
Lohana Torres <lohana100739@gmail.com>
2020-10-12 14:10:19 -0300

    Add landing structure and styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello NLW</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="happy"/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Rio de Janeiro</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
