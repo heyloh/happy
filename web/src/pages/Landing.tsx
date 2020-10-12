5a0f62f1394068f21a64e4b7f70440de03e673e8
Lohana Torres <lohana100739@gmail.com>
2020-10-12 15:10:36 -0300

    Reformat landing

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiArrowRight } from 'react-icons/fi';
+
+import '../styles/pages/landing.css';
+
+import logoImg from '../images/logo.svg';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="happy"/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Rio de Janeiro</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
