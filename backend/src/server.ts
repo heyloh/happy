d6d5fcc7f0b4907dcbf42f64bc32d85454fb859e
Lohana Torres <lohana100739@gmail.com>
2020-10-13 11:57:04 -0300

    Add error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
 
+import errorHandler from './errors/handler';
+
 const app =  express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
-
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
 
