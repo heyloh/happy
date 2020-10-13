6c77119c720f01de32dd85f5c6128890462697ff
Lohana Torres <lohana100739@gmail.com>
2020-10-13 11:21:49 -0300

    Update orphanages model

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
+
+import Image from './Image'; 
 
 @Entity('orphanages')
 
@@ -26,4 +28,10 @@ export default class Orphanage {
 
   @Column()
   open_on_weekends: boolean;
+
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
 }
