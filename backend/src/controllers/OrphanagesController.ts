6912dc3677ab0914f694d3ab3f356f8b5012af41
Lohana Torres <lohana100739@gmail.com>
2020-10-13 10:26:00 -0300

    Add orphanages controller

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return response.json(orphanage);
+  },
+
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = request.body;
+  
+    const orphanagesRepository = getRepository(Orphanage);
+  
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    });
+  
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  }
+}
