1c0e3ee063e835ca8edf1f34ed2bc822bb66b14e
Lohana Torres <lohana100739@gmail.com>
2020-10-16 19:18:10 -0300

    Add routes

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`,
+      url: `http://192.168.0.3:3333/uploads/${image.path}`,
     };
   },
 
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -8,12 +8,50 @@ const {Navigator, Screen} = createStackNavigator();
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
 
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import Header from './components/Header';
+
 export default function Routes(){
   return(
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+      <Navigator 
+        screenOptions={{ 
+          headerShown: false, 
+          cardStyle: { 
+            backgroundColor: '#F2F3F5' 
+          } 
+        }}
+      >
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanagesMap} 
+        />
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails} 
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }}
+        />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}
+        />
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData} 
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
